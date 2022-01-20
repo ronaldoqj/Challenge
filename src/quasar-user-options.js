42a1d2f29e333336a42fa68c4e702c950f79c1fc
Ana Paula Pacheco <anapaula.pacheco87@gmail.com>
2022-01-20 09:20:20 +0000

    feat: Add quasar framework

---
+++ src/quasar-user-options.js
@@ -0,0 +1,16 @@
+
+import './styles/quasar.scss'
+import iconSet from 'quasar/icon-set/material-icons-outlined.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  iconSet: iconSet
+}
